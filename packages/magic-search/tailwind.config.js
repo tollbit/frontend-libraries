475a5ddbeb5a6f3857559b55749e74176a71f587
Luke Reisch <lucas@tollbit.com>
2024-11-13 11:44:38 -0500

    Remove old tailwind stuff

--- packages/magic-search/tailwind.config.js
+++ packages/magic-search/tailwind.config.js
@@ -1,5 +1,3 @@
-import { transform } from "typescript";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   prefix: "tb-",
@@ -8,32 +6,7 @@ export default {
   },
   content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
-    extend: {
-      keyframes: {
-        slideRightIn: {
-          "0%": { transform: "" },
-          "100%": { transform: "translateX(0)" },
-        },
-        slideRightOut: {
-          "0%": { transform: "translateX(0)" },
-          "100%": { transform: "translateX(-100%)" },
-        },
-        slideLeftIn: {
-          "0%": { transform: "" },
-          "100%": { transform: "translateX(0)" },
-        },
-        slideLeftOut: {
-          "0%": { transform: "translateX(0)" },
-          "100%": { transform: "translateX(-100%)" },
-        },
-      },
-      animation: {
-        slideRightIn: "slideRightIn 0.5s ease-out forwards",
-        slideRightOut: "slideRightOut 0.5s ease-out forwards",
-        slideLeftIn: "slideLeftIn 0.5s ease-in forwards",
-        slideLeftOut: "slideLeftOut 0.5s ease-in forwards",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
