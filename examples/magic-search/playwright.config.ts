3221c84299859bd31ec8c43ea0102d1861bc349f
Luke Reisch <lucas@tollbit.com>
2024-11-19 17:18:04 -0500

    trying to remove the html reporter

--- examples/magic-search/playwright.config.ts
+++ examples/magic-search/playwright.config.ts
@@ -22,8 +22,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: [
+    [
+      "html",
+      {
+        open: process.env.CI ? "never" : "always", // if on CI then "never" otherwise "always" show
+      },
+    ],
+  ],
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
